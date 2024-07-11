a81eadb4319081b0f0230a8e30891d1295033fd6
James Corino <122735477+Jamescorino8@users.noreply.github.com>
2024-07-11 18:17:00 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,16 +31,16 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Tic-Tac-Toe game created with Flask and HTMX in Python",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "First project created with Flask and HTMX. It is a simple Tic-Tac-Toe game that can be played with a friend or against computer",
+    url: "https://github.com/Jamescorino8/TicTacToe",
   },
   {
-    title: "Web Development for Beginners",
+    title: "3 toy problems solved with python",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Three toy problems solved with python. Includes a matrix problems, a string problem, and a list problem.",
+    url: "https://github.com/Jamescorino8/MatrixArrayProblems",
   },
   {
     title: "My Resume Site",
@@ -49,9 +49,9 @@ const projectList = [
     url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "High School CS120 Final Project",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
+      "Final project for my high school computer science class. Created a simple game using Javascript. Made in colaboration with {insertnamelater}.",
     url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
   },
 ];
